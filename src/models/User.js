cb153045ac979838616f080e54588f564e64fccf
ammar-bin-quddus <ammarcghs372@gmail.com>
2025-07-28 12:00:03 +0600

    basic folder structure and user model created

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userName: { type: String },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photoUrl: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
